e4555a43eea50de844d0bfb6581a660271282075
Alec <sixers27.72@gmail.com>
2025-02-02 12:26:48 -0500

    Not sure why stuff stopped working, but this seems to address it for now

--- SupplementRatings/settings.py
+++ SupplementRatings/settings.py
@@ -33,10 +33,6 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
 CORS_ALLOWED_METHODS = [
     'GET',
     'POST',
