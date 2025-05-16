39a1cb9164fafd5db1dbe27e6a4d759e4c0b5586
Alec <sixers27.72@gmail.com>
2025-05-16 12:26:11 -0400

    More fixes

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,16 +1,49 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from .models import Supplement, Rating, Comment, Condition, Brand, UserUpvote
+import logging
+
+logger = logging.getLogger(__name__)
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': False},
+            'last_name': {'required': False},
+            'email': {'required': True}
+        }
+
+    def create(self, validated_data):
+        try:
+            user = User.objects.create_user(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                password=validated_data['password']
+            )
+            user.first_name = validated_data.get('first_name', '')
+            user.last_name = validated_data.get('last_name', '')
+            user.is_active = False  # Set user to inactive until email verification
+            user.save()
+            return user
+        except Exception as e:
+            logger.error(f"Error in RegisterUserSerializer create method: {str(e)}", exc_info=True)
+            raise
 
 
-class UserSerializer(serializers.ModelSerializer):
+class BasicUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = BasicUserSerializer(read_only=True)
     replies = serializers.SerializerMethodField()
     is_edited = serializers.BooleanField(read_only=True)
     has_upvoted = serializers.SerializerMethodField()
@@ -23,7 +56,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     def get_replies(self, obj):
         replies = Comment.objects.filter(parent_comment=obj)
-        return CommentSerializer(replies, many=True).data
+        return CommentSerializer(replies, many=True, context=self.context).data
 
     def update(self, instance, validated_data):
         instance.is_edited = True
@@ -39,7 +72,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class RatingSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = BasicUserSerializer(read_only=True)
     condition_names = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True, read_only=True)
     has_upvoted = serializers.SerializerMethodField()
@@ -55,9 +88,9 @@ class RatingSerializer(serializers.ModelSerializer):
         return [condition.name for condition in obj.conditions.all()]
 
     def create(self, validated_data):
-        conditions = validated_data.pop('conditions', [])
+        conditions_data = validated_data.pop('conditions', [])
         rating = Rating.objects.create(**validated_data)
-        rating.conditions.set(conditions)
+        rating.conditions.set(conditions_data)
         return rating
 
     def get_has_upvoted(self, obj):
@@ -68,13 +101,17 @@ class RatingSerializer(serializers.ModelSerializer):
 
 
 class SupplementSerializer(serializers.ModelSerializer):
-    ratings = RatingSerializer(many=True, read_only=True)
+    ratings = serializers.SerializerMethodField()
     avg_rating = serializers.FloatField(read_only=True)
     rating_count = serializers.IntegerField(read_only=True)
     
     class Meta:
         model = Supplement
         fields = ['id', 'name', 'category', 'dosage_unit', 'ratings', 'avg_rating', 'rating_count']
+
+    def get_ratings(self, obj):
+        ratings_queryset = obj.ratings.all()
+        return RatingSerializer(ratings_queryset, many=True, context=self.context).data
 
 
 class ConditionSerializer(serializers.ModelSerializer):
